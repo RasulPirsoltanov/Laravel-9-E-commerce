c6a7d8bd98a894d49cc561dceb9485a228bb58e8
RasulPirsoltanov <resulresull510@gmail.com>
2022-09-01 21:36:22 +0400

    firrst commit

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="@yield('keywords')" name="keywords">
+    <meta content="@yield('description')" name="description">
+
+    <!-- Favicon -->
+    <link href="{{ asset('assets/img/favicon.ico') }}" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{ asset('assets/lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    @yield('css')
+    @yield('headerjs')
+
+</head>
+
+<body>
+    
+    @include('home._header')
+    <div id="navigation">
+        <div id="responsive-nav">
+            @include('home._navbar')
+        </div>
+    </div>
+    @include('home._slider')
+    @yield('content')
+    @include('home._footer')
+</body>
+@yield('footerjs')
+</html>
